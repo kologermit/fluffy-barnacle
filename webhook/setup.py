4e19c466b6130305053aef1048e64f8c6193d9d6
Feodor <kologermit@gmail.com>
2023-12-24 01:51:57 +0400

    Fix3

--- webhook/setup.py
+++ webhook/setup.py
@@ -1,20 +1,34 @@
 from aiohttp import web
-import logging, aiohttp
+import logging, aiohttp, prodamuspy
 from loader import dp
+from data.config import prodamus as conf
 
 async def handle(request: aiohttp.web_request.Request):
     try:
         headers = request.headers
-        user_id = headers.get("_param_user_tg_id")
-        print("Headers", request.headers.items())
-        print("Post", (await request.post()).items())
-        if not user_id:
-            return web.Response(text='User id napam not found')
-        try:
-            await dp.bot.send_message(user_id, "Оплата принята")
-        except Exception as err:
-            logging.exception(err)
-            return web.Response(text='Failed to send message')
+        post = await request.post()
+        prodamus = prodamuspy.ProdamusPy(conf["token"])
+        bodyDict = dict(headers)
+        checkSign = prodamus.sign(bodyDict)
+        if not checkSign:
+            return web.Response(text="Uncheck")
+        signIsGood = prodamus.verify(bodyDict, checkSign)
+        if signIsGood:
+            print("Signature is awesome")
+        else:
+            print("Signature is incorrect")
+        print("Headers", headers)
+        print("Post", post)
+        # if post.get("_param") is None:
+        #     return  web.Response(text='Sign param not found')
+
+        # if not user_id:
+        #     return web.Response(text='User id napam not found')
+        # try:
+        #     await dp.bot.send_message(user_id, "Оплата принята")
+        # except Exception as err:
+        #     logging.exception(err)
+        #     return web.Response(text='Failed to send message')
         return web.Response(text="success webhook")
     except Exception as err:
         logging.exception(err)
