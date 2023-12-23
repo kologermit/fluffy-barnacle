471efb1517ce90ab4b31723df6642f51502473fa
Feodor <kologermit@gmail.com>
2023-12-23 23:00:08 +0400

    Проверка параментров вебхука

--- webhook/setup.py
+++ webhook/setup.py
@@ -1,14 +1,22 @@
 from aiohttp import web
-import logging
+import logging, aiohttp
 
-async def handle(request):
-    logging.info(request)
-    text = 'Hello, world'
-    return web.Response(text=text)
+async def handle(request: aiohttp.web_request.Request):
+    post = await request.post()
+    for key, value in post.items():
+        logging.info("-------------------")
+        logging.info("key") 
+        logging.info(key)
+        logging.info("value") 
+        logging.info(value)
+        logging.info("-------------------")
+    
+    return web.Response(text='Hello world')
 
 async def setup_webhook():
     app = web.Application()
     app.router.add_get('/', handle)
+    app.router.add_post('/', handle)
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, host="0.0.0.0", port=80)  
