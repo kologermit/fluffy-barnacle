d1fd9478718de8302099f134bde90fbc09fca687
Feodor <kologermit@gmail.com>
2023-12-24 01:00:42 +0400

    Изменил post на header

--- webhook/setup.py
+++ webhook/setup.py
@@ -4,32 +4,20 @@ from loader import dp
 
 async def handle(request: aiohttp.web_request.Request):
     try:
-        print("Output")
-        post = await request.headers()
-        for key, value in post.items():
-            print("Key", key, "Value", value)
-        order_id = post.get("order_id", "-1")
-        order_num = post.get("order_num", "-1")
-        user_id = post.get("_param_user_tg_id")
+        headers = request.headers
+        user_id = headers.get("_param_user_tg_id")
+        print(user_id)
         if not user_id:
-            msg = 'User id napam not found'
-            print(msg)
-            return web.Response(text=msg)
+            return web.Response(text='User id napam not found')
         try:
             await dp.bot.send_message(user_id, "Оплата принята")
         except Exception as err:
             logging.exception(err)
-            msg = 'Failed to send message'
-            print(msg)
-            return web.Response(text=msg)
-        msg = "success webhook"
-        print(msg)
-        return web.Response(text=msg)
+            return web.Response(text='Failed to send message')
+        return web.Response(text="success webhook")
     except Exception as err:
         logging.exception(err)
-        msg = "server Error"
-        print(msg)
-        return web.Response(text=msg)
+        return web.Response(text="server Error")
 
 async def setup_webhook():
     app = web.Application()
