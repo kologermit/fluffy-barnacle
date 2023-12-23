1e2aaea3b1ad9052e0e024e7a803516e181a17c3
Feodor <kologermit@gmail.com>
2023-12-24 00:33:09 +0400

    Добавил параметры в запрос продамуса

--- prodamus/prodamus.py
+++ prodamus/prodamus.py
@@ -7,9 +7,13 @@ def prodamus_create_url(product: dict, user_id: int):
         "order_id": user_id,
         "demo_mode": prodamusConf["demo_mode"],
         "callbackType": "json",
+        "customer_email": "kologermit@gmail.com",
+        "customer_extra": "extra",
         "currency": "rub",
         "payments_limit": 1,
-        "_param_user_tg_id": user_id
+        "_param_user_tg_id": user_id,
+        "callbackType": "json",
+        "payments_limit": "1"
     }
     for key, value in product.items():
         data[f"products[0][{key}]"] = value
