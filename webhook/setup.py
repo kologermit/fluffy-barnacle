657e8467df145fe7a2961a76252faae07834fcaf
Feodor <kologermit@gmail.com>
2023-12-24 00:43:14 +0400

    Добавил вывод параметров

--- webhook/setup.py
+++ webhook/setup.py
@@ -4,6 +4,7 @@ from loader import dp
 
 async def handle(request: aiohttp.web_request.Request):
     try:
+        print("Output")
         post = await request.post()
         for key, value in post.items():
             print("Key", key, "Value", value)
@@ -11,16 +12,24 @@ async def handle(request: aiohttp.web_request.Request):
         order_num = post.get("order_num", "-1")
         user_id = post.get("_param_user_tg_id")
         if not user_id:
-            return web.Response(text='User id napam not found')
+            msg = 'User id napam not found'
+            print(msg)
+            return web.Response(text=msg)
         try:
             await dp.bot.send_message(user_id, "Оплата принята")
         except Exception as err:
             logging.exception(err)
-            return web.Response(text='Failed to send message')
-        return web.Response(text='Success webhook')
+            msg = 'Failed to send message'
+            print(msg)
+            return web.Response(text=msg)
+        msg = "success webhook"
+        print(msg)
+        return web.Response(text=msg)
     except Exception as err:
         logging.exception(err)
-        return web.Response(text="Server Error")
+        msg = "server Error"
+        print(msg)
+        return web.Response(text=msg)
 
 async def setup_webhook():
     app = web.Application()
